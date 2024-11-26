942bf91723b5c5ec18eece807501415a16cfde43
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-26 18:01:46 +0530

    imported and linked cart compoenent in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Register from './components/login_register/Register'
 import Header from './components/Header_Footer/Header'
 import Footer from './components/Header_Footer/Footer'
 import RestaurantList from './components/RestaurantList'
+import Cart from './components/Cart/Cart';
 const App = () => {
   return (
     <div className='App'>
