21a0b5c1505a0ff6293944baadf9f1bb11a32c07
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-26 17:44:35 +0530

    footer component is imported in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './components/login_register/Login'
 import Register from './components/login_register/Register'
 import Header from './components/Header_Footer/Header'
+import Footer from './components/Header_Footer/Footer'
 
 const App = () => {
   return (
