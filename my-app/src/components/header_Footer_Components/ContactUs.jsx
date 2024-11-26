e242819d1fb3969f52faad30d46c2acc7777a086
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-26 18:33:28 +0530

    ContactUs component is added

---
+++ my-app/src/components/header_Footer_Components/ContactUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+  return (
+    <div className="contact-us">
+      <h2 className="contact-title">Contact Us</h2>
+      <p>If you have any questions, feel free to reach out to us!</p>
+      <p>Email: support@fooddeliveryapp.com</p>
+      <p>Phone: +1 (555) 123-4567</p>
+    </div>
+  );
+};
+
+export default ContactUs;
