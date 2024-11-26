9c7a27f52d71e0bd974d799f4dbd280257ca6f01
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-26 17:35:21 +0530

    Header imported in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Login from './components/login_register/Login'
 import Register from './components/login_register/Register'
+import Header from './components/login_register/Header_Footer/Header'
 
 const App = () => {
   return (
     <div className='App'>
-      {/* <Header/> */}
+      <Header/>
         <Login/>
         <Register/>
       {/* <Footer/> */}
