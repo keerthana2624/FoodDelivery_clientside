001b329a76f0f45006d8c995c2cf7606099d1138
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-26 18:41:46 +0530

    termsOfServices component is implemented

---
+++ my-app/src/components/header_Footer_Components/TermsOfServices.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TermsOfServices.css';
+
+const TermsOfService = () => {
+  return (
+    <div className="terms-of-service">
+      <h2 className="terms-title">Terms of Service</h2>
+      <p>By using our services, you agree to our terms and conditions.</p>
+    </div>
+  );
+};
+
+export default TermsOfService;
