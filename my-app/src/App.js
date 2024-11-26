82f1ecde683fc8fa8ade21c5c3aaecbc734a6936
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-26 18:16:48 +0530

    modified

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import './App.css'
+import './App.css'
 import Header from './components/header_footer/Header'
 import Footer from './components/header_footer/Footer'
 import RestaurantList from './components/RestaurantList'
@@ -9,6 +9,7 @@ import Cart from './components/Cart/Cart';
 import { CartProvider } from './components/Cart/CartContext';
 // import Login from './components/login_register/Login'
 // import Register from './components/login_register/Register'
+import Checkout from './components/Checkout/Checkout';
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/" element={<RestaurantList />} />
           <Route path="/menu/:restaurantName" element={<Menu />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
         <Footer/>
       </Router>
