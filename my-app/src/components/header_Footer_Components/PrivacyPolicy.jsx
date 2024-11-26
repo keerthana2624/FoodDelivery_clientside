9fd950c2a7a48e10121a43f6e11e6f2d476daefa
keerthana2624 <keerthanamummy2002@gmail.com>
2024-11-26 18:34:53 +0530

    privacyPolicy component is implemented

---
+++ my-app/src/components/header_Footer_Components/PrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PrivacyPolicy.css';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="privacy-policy">
+      <h2 className="policy-title">Privacy Policy</h2>
+      <p>Your privacy is important to us. We ensure that your personal data is handled with care.</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
